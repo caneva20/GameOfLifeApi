0a916df0a5608404f4f329f917fef93cd1d41bc6
Fabricio Caneva <hardlymersk@gmail.com>
2025-03-04 20:33:11 -0300

    feat: add GameRepository

---
+++ src/GameOfLife.Abstractions/Repositories/IGameRepository.cs
@@ -0,0 +1,12 @@
+﻿using GameOfLife.Models;
+
+namespace GameOfLife.Repositories;
+
+public interface IGameRepository {
+    /// <summary>
+    /// Creates a game board
+    /// </summary>
+    /// <param name="board">The board with initial configuration to be created</param>
+    /// <returns>The created board</returns>
+    Task<Board> CreateBoard(Board board);
+}
