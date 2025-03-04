0ba20ca43a6d455a1ab41cf4c0a156b295ee55e0
Fabricio Caneva <hardlymersk@gmail.com>
2025-03-04 20:35:12 -0300

    feat: add create board endpoint

---
+++ src/GameOfLife.Api/Controllers/GameController.cs
@@ -0,0 +1,39 @@
+﻿using GameOfLife.Models;
+using GameOfLife.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameOfLife.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GameController : ControllerBase {
+    private readonly ILogger<GameController> _logger;
+
+    private readonly ICreateBoardUseCase _createBoardUseCase;
+
+    public GameController(ILogger<GameController> logger, ICreateBoardUseCase createBoardUseCase) {
+        _logger = logger;
+        _createBoardUseCase = createBoardUseCase;
+    }
+
+    //TODO: Add DTO
+    /// <summary>
+    /// Creates a board
+    /// </summary>
+    /// <param name="board">The board to create with initial state</param>
+    /// <returns>The created board</returns>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateBoard([FromBody] Board board) {
+        _logger.LogInformation("Creating board");
+
+        var createdBoard = await _createBoardUseCase.CreateBoard(board);
+
+        if (createdBoard == null) {
+            return BadRequest();
+        }
+
+        return Ok(); //TODO: Return 201
+    }
+}
