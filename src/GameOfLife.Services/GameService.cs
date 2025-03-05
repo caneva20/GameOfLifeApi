59fdff2c1cb67e7b5ecfe6431648080e63e08e72
Fabricio Caneva <hardlymersk@gmail.com>
2025-03-05 19:55:24 -0300

    feat: add game simulation

---
+++ src/GameOfLife.Services/GameService.cs
@@ -0,0 +1,90 @@
+﻿using GameOfLife.Models;
+
+namespace GameOfLife.Services;
+
+public sealed class GameService : IGameService {
+    private static readonly int[] NeighbourOffsets = { -1, 0, 1 };
+
+    public BoardState BuildState(Board board, int maxStateHistory) {
+        var state = new BoardState(maxStateHistory);
+
+        UpdateLiveCells(state, board.LiveCells.Select(x => (x.X, x.Y)).ToList());
+        UpdateNeighbours(state);
+
+        return state;
+    }
+
+    public void Simulate(BoardState state, int iterations) {
+        for (var i = 0; i < iterations; i++) {
+            var nextCells = new List<(int x, int y)>();
+
+            foreach (var (cell, neighborCount) in state.Neighbors) {
+                var isAlive = IsAlive(state, cell);
+
+                if (isAlive && neighborCount is 2 or 3) {
+                    nextCells.Add(cell);
+                }
+
+                if (!isAlive && neighborCount is 3) {
+                    nextCells.Add(cell);
+                }
+            }
+
+            UpdateLiveCells(state, nextCells);
+            UpdateNeighbours(state);
+        }
+    }
+
+    public bool IsAlive(BoardState state, (int x, int y) cell) {
+        return state.LiveCells.Contains(cell);
+    }
+
+    public bool IsComplete(BoardState state) {
+        return state.HashHistory.Contains(ComputeHash(state));
+    }
+
+    public int ComputeHash(BoardState state) {
+        var hash = 17;
+        foreach (var cell in state.LiveCells) {
+            hash = hash * 31 + cell.x;
+            hash = hash * 31 + cell.y;
+        }
+
+        return hash;
+    }
+
+    public void UpdateLiveCells(BoardState state, List<(int x, int y)> newCells) {
+        state.HashHistory.Enqueue(ComputeHash(state));
+        if (state.HashHistory.Count > state.MaxStateHistory) {
+            state.HashHistory.Dequeue();
+        }
+
+        state.LiveCells.Clear();
+
+        foreach (var cell in newCells.OrderBy(x => x.x).ThenBy(x => x.y)) {
+            state.LiveCells.Add(cell);
+        }
+    }
+
+    public void UpdateNeighbours(BoardState state) {
+        state.Neighbors.Clear();
+
+        foreach (var cell in state.LiveCells) {
+            UpdateNeighbours(state, cell);
+        }
+    }
+
+    private static void UpdateNeighbours(BoardState state, (int x, int y) cell) {
+        foreach (var dx in NeighbourOffsets) {
+            foreach (var dy in NeighbourOffsets) {
+                if (dx == 0 && dy == 0) {
+                    continue;
+                }
+
+                var neighbour = (cell.x + dx, cell.y + dy);
+                state.Neighbors.TryAdd(neighbour, 0);
+                state.Neighbors[neighbour]++;
+            }
+        }
+    }
+}
---
+++ src/GameOfLife.Services/IGameService.cs
@@ -0,0 +1,58 @@
+﻿using GameOfLife.Models;
+
+namespace GameOfLife.Services;
+
+/// <summary>
+/// A service that provides functionality for the Game of Life
+/// </summary>
+public interface IGameService {
+    /// <summary>
+    /// Builds the initial state of the game from a board
+    /// </summary>
+    /// <param name="board">The board to build the state from</param>
+    /// <param name="maxStateHistory">The maximum amount of history to keep</param>
+    /// <returns>The initial state of the game</returns>
+    BoardState BuildState(Board board, int maxStateHistory);
+
+    /// <summary>
+    /// Simulates the game for a set amount of iterations
+    /// </summary>
+    /// <param name="state">The current state of the game</param>
+    /// <param name="iterations">The amount of iterations to simulate</param>
+    void Simulate(BoardState state, int iterations);
+
+    /// <summary>
+    /// Checks if a cell is alive in the current state
+    /// </summary>
+    /// <param name="state">The current state of the game</param>
+    /// <param name="cell">The cell to check</param>
+    /// <returns>True if the cell is alive, false otherwise</returns>
+    bool IsAlive(BoardState state, (int x, int y) cell);
+
+    /// <summary>
+    /// Updates the neighbours for all cells in the state
+    /// </summary>
+    /// <param name="state">The current state of the game</param>
+    void UpdateNeighbours(BoardState state);
+
+    /// <summary>
+    /// Updates the alive cells in the state
+    /// </summary>
+    /// <param name="state">The current state of the game</param>
+    /// <param name="newCells">The new alive cells</param>
+    void UpdateLiveCells(BoardState state, List<(int x, int y)> newCells);
+
+    /// <summary>
+    /// Checks if the game has reached a stable state
+    /// </summary>
+    /// <param name="state">The current state of the game</param>
+    /// <returns>True if the game has reached a stable state, false otherwise</returns>
+    bool IsComplete(BoardState state);
+
+    /// <summary>
+    /// Computes a hash for the current state of the game
+    /// </summary>
+    /// <param name="state">The current state of the game</param>
+    /// <returns>The hash of the current state</returns>
+    int ComputeHash(BoardState state);
+}
